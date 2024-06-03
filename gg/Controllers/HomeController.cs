07b1f8a9ab1af0b14513f0d6d028feefc4795265
Zhenya Obukhov <obukhovkostya@gmail.com>
2024-06-03 17:18:05 +0500

    initial commit

---
+++ gg/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using gg.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gg.Controllers
+
+{
+    
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public string Index(string First_name, string Last_name, string Middle_name, string school_middle, string school_new, string quantum, bool klass, bool Fruit)
+        {
+            return $"First_name: {First_name}\n" +
+                $"Last_name: {Last_name}\n" +
+                $"Middle_name: {Middle_name}\n" +
+                $"school_new: {school_new}\n" +
+                $"quantum: {quantum}\n" +
+                $"10_class: {klass}\n" +
+                $"da_net: {Fruit}\n";
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+    
---
+++ gg/wwwroot/js/site.js
@@ -0,0 +1,62 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+$(document).ready(function () {
+	$("#optionsRadios1").change(function () {
+		$('.hide').fadeIn().show();
+	});
+
+	$("#optionsRadios2").change(function () {
+		$('.hide').fadeOut(300);
+	});
+})
+
+
+let activeFilters = [];
+
+function filterSelection(filter) {
+    const allBtns = document.querySelectorAll("#myBtnContainer .btn");
+
+    if (filter === 'all') {
+        activeFilters = [];
+        allBtns.forEach(btn => btn.classList.remove('active'));
+        document.querySelector(".btn[data-filter='all']").classList.add('active');
+    } else {
+        const btn = document.querySelector(`.btn[data-filter="${filter}"]`);
+        const index = activeFilters.indexOf(filter);
+
+        if (index > -1) {
+            activeFilters.splice(index, 1);
+            btn.classList.remove('active');
+        } else {
+            activeFilters.push(filter);
+            btn.classList.add('active');
+        }
+
+        document.querySelector(".btn[data-filter='all']").classList.remove('active');
+    }
+
+    showFilteredDivs();
+}
+
+function showFilteredDivs() {
+    const divs = document.querySelectorAll(".filterDiv");
+
+    divs.forEach(div => {
+        const classList = Array.from(div.classList);
+        const matches = activeFilters.every(filter => classList.includes(filter));
+
+        if (matches || activeFilters.length === 0) {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
+    });
+}
+
+// Initial call to show all divs
+showFilteredDivs();
